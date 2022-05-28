dca4f72d336dcc8780fb87fc60ed058b3ed8cf76
Rok <roky.mlakar@gmail.com>
2022-05-28 15:21:58 +0200

    home categories bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import MainContainer from "../components/Containers/MainContainer";
 import SearchBar from "../components/homeComponents/SearchBar";
 import { Title } from "../components/Titles/Titles";
+import CategoryCard from "../components/Cards/CategoryCard";
 
 import styles from '../styles/homeComponents/Home.module.scss';
 
-function Home() {
+const Home = () => {
     return (
         <MainContainer optionClass={styles.container}>
             <div className={styles.main}>
@@ -17,7 +18,12 @@ function Home() {
                 {/* CATEGORIES */}
                 <div className={styles.categories}>
                     <Title>Categories Last 30 Days</Title>
-                    <div className={styles.content}>{/* ctgs */}</div>
+                    <div className={styles.content}>
+                        <CategoryCard/>
+                        <CategoryCard/>
+                        <CategoryCard/>
+                        <CategoryCard/>
+                    </div>
                 </div>
             </div>
         </MainContainer>
