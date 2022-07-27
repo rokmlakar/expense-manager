8c93274aca4527288d78953cb923004adbf028e7
Rok <roky.mlakar@gmail.com>
2022-07-27 16:28:46 +0200

    auth rout iupdate

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -6,5 +6,6 @@ router.get('/user', authController.auth_user);
 router.post('/auth', authController.auth_login);
 router.post('/logout', authController.auth_logout);
 router.post('/register', authController.auth_register);
+router.get('/verify-email', authController.auth_verify)
 
 module.exports = router;
